b113c7a5308758fc6d8fce1a84833a224658d288
student <student@itismeucci.com>
2026-03-17 13:36:01 +0100

    php controller

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/AlunniController.php';
+require __DIR__ . '/controllers/TransactionsController.php';
 
 $app = AppFactory::create();
 
@@ -19,7 +19,14 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     $response->getBody()->write("Hello, $name");
     return $response;
 });
-
-$app->get('/alunni', "AlunniController:index");
+/*
+GET /accounts/1/transactions per ottenere l'elenco dei movimenti
+GET /accounts/1/transactions/5 per ottenere il dettaglio di un movimento
+POST /accounts/1/deposits per registrare un deposito
+POST /accounts/1/withdrawals per registrare un prelievo
+PUT /accounts/1/transactions/5 per modificare la descrizione di un movimento
+DELETE /accounts/1/transactions/5 per eliminare un movimento secondo la regola scelta
+*/
+$app->get('/accounts/{id}', "/transactions");
 
 $app->run();
